a57742280a506e1fad6c0f30ccf44b519930dbf2
Thomas Chen <42944780+MemeLords1@users.noreply.github.com>
2019-10-18 17:57:32 -0400

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -1,26 +1,12 @@
-
 public class Apple extends Fruit {
-   
-   
-   
    public Apple(String name, String taste, double size){
-   
-       super(name,taste,size);
-
+      super(name,taste,size);
      }
-
-@Override
    public String eat(){
-     return name + " is " + taste;
-//return name ;
-
+      return name + " is " + taste;
 }
  public static void main(String [] args){
- //System.out.println(super(name));
  Apple myA=new  Apple("APPLE "," has flavor",54.87);
  System.out.println(myA.eat());
- 
- 
       } 
-
 }

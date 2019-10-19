c3c25fe3166f0bbc3b270b1813e0669bb1a3a443
Thomas Chen <42944780+MemeLords1@users.noreply.github.com>
2019-10-18 21:35:52 -0400

    Update Fruit.java

--- Fruit.java
+++ Fruit.java
@@ -1,18 +1,19 @@
+//Thomas Chen
+//Cisc 3115
+//Assignment #3
+
 public class Fruit {
    String name;
    String taste;
-   double size;
-   public Fruit(String name, String taste, double size){
+   int size;
+   public Fruit(String name, String taste, int size){
       this.name = name;
       this.taste = taste;
       this.size = size;
 }
 public void getFruit(){
-//return name;
-//return size;
    }
 public String eat(){
-//return name + " is " + taste;
 return taste + "is very Good" ;
 }
 public static void  main (String [] args){
